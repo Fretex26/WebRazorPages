4b1d0f5091041a69bc8f86624cf8d349a0f16e5f
angela.brunst <angela.hairstylist@gmail.com>
2022-10-30 21:07:26 -0600

    Created DeskQuote Class

---
+++ MegaDesk/MegaDesk/Models/DeskQuote.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace MegaDesk.Models
+{
+    public class DeskQuote
+    {
+        public int ID { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Entry Date")]
+        public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Customer Name")]
+        [Required]
+        public string CustomerName { get; set; }
+
+        [Display(Name = "Surface Material")]
+        [Required]
+        public string SurfaceMaterial { get; set; }
+
+        [Display(Name = "Number of Drawers")]
+        [Required]
+        public int NumOfDrawers { get; set; }
+
+        [Display(Name = "Desk Width")]
+        [Required]
+        public int Width { get; set; }
+
+        [Display(Name = "Desk Depth")]
+        [Required]
+        public int Depth { get; set; }
+
+        [Display(Name = "Rush Order Needed")]
+        [Required]
+        public int RushDays { get; set; }
+    }
+}
