74c08a96f45bd52268a02589abae5657dbe9af6b
angela.brunst <angela.hairstylist@gmail.com>
2022-11-01 16:52:36 -0600

    Added scaffolding

---
+++ MegaDesk/MegaDesk/Migrations/20221101221314_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MegaDesk.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DeskQuote",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuoteDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CustomerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Width = table.Column<int>(type: "int", nullable: false),
+                    Depth = table.Column<int>(type: "int", nullable: false),
+                    NumOfDrawers = table.Column<int>(type: "int", nullable: false),
+                    SurfaceMaterial = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductionTime = table.Column<int>(type: "int", nullable: false),
+                    Area = table.Column<int>(type: "int", nullable: false),
+                    QuoteTotal = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeskQuote", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeskQuote");
+        }
+    }
+}
--- MegaDesk/MegaDesk/Models/DeskQuote.cs
+++ MegaDesk/MegaDesk/Models/DeskQuote.cs
@@ -8,31 +8,39 @@ namespace MegaDesk.Models
         public int ID { get; set; }
 
         [DataType(DataType.Date)]
-        [Display(Name = "Entry Date")]
-        public DateTime DateAdded { get; set; }
+        [Display(Name = "Quote Date")]
+        public DateTime QuoteDate { get; set; }
 
         [Display(Name = "Customer Name")]
         [Required]
         public string CustomerName { get; set; }
 
-        [Display(Name = "Surface Material")]
-        [Required]
-        public string SurfaceMaterial { get; set; }
-
-        [Display(Name = "Number of Drawers")]
-        [Required]
-        public int NumOfDrawers { get; set; }
-
         [Display(Name = "Desk Width")]
+        [Range(24, 96)]
         [Required]
         public int Width { get; set; }
 
         [Display(Name = "Desk Depth")]
+        [Range(12, 48)]
         [Required]
         public int Depth { get; set; }
 
-        [Display(Name = "Rush Order Needed")]
+        [Display(Name = "Number of Drawers")]
+        [Range(0, 7)]
         [Required]
-        public int RushDays { get; set; }
+        public int NumOfDrawers { get; set; }
+
+        [Display(Name = "Surface Material")]
+        [Required]
+        public string SurfaceMaterial { get; set; }
+
+        [Display(Name = "Production Time")]
+        [Required]
+        public int ProductionTime { get; set; }
+
+        public int Area { get; set; }
+
+        [Display(Name = "Quote Total")]
+        public decimal QuoteTotal { get; set; }
     }
 }
