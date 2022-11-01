a2140d3a54a547c06b5090fd21930a7ef0467af0
angela.brunst <angela.hairstylist@gmail.com>
2022-11-01 17:37:29 -0600

    Created SeedData document

---
+++ MegaDesk/MegaDesk/Models/SeedData.cs
@@ -0,0 +1,41 @@
+﻿using MegaDesk.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MegaDesk.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MegaDeskContext(serviceProvider.GetRequiredService<
+                    DbContextOptions<MegaDeskContext>>()))
+            {
+                if (context == null || context.DeskQuote == null)
+                {
+                    throw new InvalidOperationException("Null DeskQuoteContext");
+                }
+
+                // Look for any desk quotes
+                if (context.DeskQuote.Any())
+                {
+                    return;
+                }
+
+                context.DeskQuote.AddRange(
+                    new DeskQuote
+                    {
+                        QuoteDate = DateTime.Now,
+                        CustomerName = "Jane Doe",
+                        Width = 24,
+                        Depth = 12,
+                        NumOfDrawers = 1,
+                        SurfaceMaterial = "Laminate",
+                        ProductionTime = 3,
+                        Area = 288,
+                        QuoteTotal = 410
+                    }
+                    );
+            }
+        }
+    }
+}
